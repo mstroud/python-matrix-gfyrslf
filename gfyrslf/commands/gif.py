28ad0e36bca4bfdbab6bb6d7ca4a9c1a9d6e7fd8
Matt Stroud <matt.c.stroud@gmail.com>
2019-04-18 23:47:47 -0500

    Testing tenor API integration

--- gfyrslf/commands/gif.py
+++ gfyrslf/commands/gif.py
@@ -1,5 +1,7 @@
 import logging
 import requests
+import json
+import urllib
 
 import giphy_client
 from giphy_client.rest import ApiException
@@ -24,13 +26,15 @@ class GifCommand(GfyrslfCommand):
         self.lang = 'en'  # str | Specify default country for regional content; use a 2-letter ISO 639-1 country code. See list of supported languages <a href = \"../language-support\">here</a>. (optional)
         self.fmt = 'json'  # str | Used to indicate the expected response format. Default is Json. (optional) (default to json)
 
-
     def event_handler(self, bot, room, event):
         # Parse command args. TODO: Add helper function to parent class for arg parsing
         args = event['content']['body'].split()
         args.pop(0)
         query = ' '.join(args)
+        #return self.event_handler_giphy(bot, room, query)
+        return self.event_handler_tenor(bot, room, query)
 
+    def event_handler_giphy(self, bot, room, query):
         # Give the people a GIF
         try:
             # Search Giphy Endpoint
@@ -54,4 +58,49 @@ class GifCommand(GfyrslfCommand):
 
         # Send the media link to the room
         room.send_image(mxc_url, api_response.data[0].slug + '(' + response.headers['Content-Type'] + ')',
+                        mimetype=response.headers['Content-Type'])
+
+    def event_handler_tenor(self, bot, room, query):
+        # set the apikey and limit
+        apikey = "LIVDSRZULELA"  # test value
+
+        # load the user's anonymous ID from cookies or some other disk storage
+        # anon_id = <from db/cookies>
+
+        # ELSE - first time user, grab and store their the anonymous ID
+        r = requests.get("https://api.tenor.com/v1/anonid?key=%s" % apikey)
+
+        if r.status_code == 200:
+            anon_id = json.loads(r.content)["anon_id"]
+            # store in db/cookies for re-use later
+        else:
+            anon_id = ""
+
+        # get the top GIFs for the search term
+        query_dict = {
+            'q': query,
+            'locale': 'en_US',
+            'key': apikey,
+            'limit':self.limit,
+            'anon_id':anon_id,
+            'contentfilter':'off',
+            'media_filter':'minimal'
+        }
+        qs = urllib.parse.urlencode(query_dict)
+        r = requests.get("https://api.tenor.com/v1/search?{}".format(qs))
+
+        if r.status_code != 200:
+            logging.warning("Status not OK: {}".format(r.status_code))
+            return
+
+        # Take the first hit and upload it to the Matrix server
+        api_result = json.loads(r.content)['results'][0]
+
+        url = api_result['media'][0]['gif']['url']
+        slug = api_result['title'] + api_result['id']
+        response = requests.get(url, stream=False)
+        mxc_url = bot.client.upload(response.content, response.headers['Content-Type'])
+
+        # Send the media link to the room
+        room.send_image(mxc_url, slug + '(' + response.headers['Content-Type'] + ')',
                         mimetype=response.headers['Content-Type'])
