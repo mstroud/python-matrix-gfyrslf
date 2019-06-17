ef3d87972bd7f87d2caaeea1e571fbbd187b5cfa
Matt Stroud <matt.c.stroud@gmail.com>
2019-06-17 10:01:06 -0500

    Whitespace cleanup, merging quotes feature

--- gfyrslf/commands/__init__.py
+++ gfyrslf/commands/__init__.py
@@ -3,3 +3,4 @@ from gfyrslf.commands.help import HelpCommand
 from gfyrslf.commands.dice_roll import DiceCommand
 from gfyrslf.commands.hi import HelloCommand
 from gfyrslf.commands.gif import GifCommand
+from gfyrslf.commands.quotes import QuoteCommand
